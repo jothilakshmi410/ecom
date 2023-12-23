f8589c0a94a9072d56ec92c5b586c291b17f8e97
jothilakshmi410 <jothi410013@gmail.com>
2023-12-23 18:17:19 +0530

    Updates

---
+++ precache-manifest.d1e0382ad8567d9ce2f2edaa04e3d119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "223443f491104e70328e",
+    "url": "/ecom/static/css/main.2bd43a4e.chunk.css"
+  },
+  {
+    "revision": "223443f491104e70328e",
+    "url": "/ecom/static/js/main.223443f4.chunk.js"
+  },
+  {
+    "revision": "3532a535b7a04ff3784d",
+    "url": "/ecom/static/js/1.3532a535.chunk.js"
+  },
+  {
+    "revision": "316e1fec650fba2e535d",
+    "url": "/ecom/static/js/runtime~main.316e1fec.js"
+  },
+  {
+    "revision": "a0bb7870a94e7b1087c5003fbd651a7e",
+    "url": "/ecom/index.html"
+  }
+];
